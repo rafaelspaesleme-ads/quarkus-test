4dddb3964de9ab13a99a027a638d1047abe945aa
Rafael Paes Leme <rafael.leme@ntendencia.com.br>
2020-02-14 17:58:03 -0300

    up project

---
+++ src/test/java/dev/horochovec/quarkus/NativeMyResourceIT.java
@@ -0,0 +1,9 @@
+package dev.horochovec.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeMyResourceIT extends MyResourceTest {
+
+    // Execute the same tests but in native mode.
+}
